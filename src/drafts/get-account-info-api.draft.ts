c8f53ccce0741805ba8d0d1e1bb46e28f201a28d
matmoimjs <matmoimjs@gmail.com>
2021-12-07 04:14:18 +0200

    commit

---
+++ src/drafts/get-account-info-api.draft.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { ENDPOINTS, HOST_URL } from '../common/api.constants';
+import { SignGenerator } from '../common/sign-generator';
+
+export type AccountInfoResDto = {
+    id: string,
+    currency: string,
+    type: 'main' | 'trade' | 'margin' | 'pool',
+    balance: string,
+    available: string,
+    holds: string,
+}
+
+class AccountApi {
+    public static async getAccountInfo(params?: {
+        type?: 'main' | 'trade' | 'margin',
+        currency?: string
+    }):
+  Promise<AccountInfoResDto[]> {
+        const { endpoint, method } = ENDPOINTS['GET /api/v1/accounts'];
+        const headers = SignGenerator
+            .create()
+            .generateHeaders(
+                method,
+                endpoint,
+                {
+                    params,
+                },
+            );
+
+        const { data: axiosData } = await axios({
+            params,
+            headers,
+            method,
+            url: HOST_URL + endpoint,
+        });
+        const { data } = axiosData;
+
+        return data;
+    }
+}
+
+export const {
+    getAccountInfo,
+} = AccountApi;
